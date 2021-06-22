b28d0f7cc636b06b19c172c023a802a7aa426226
dylanfisher <hi@dylanfisher.com>
2021-06-21 20:19:57 -0400

    first commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+ruby '3.0.1'
+
+gem 'forest', '~> 2.0', git: 'https://github.com/dylanfisher/forest.git'
+
+gem 'rails', '~> 6.1'
+gem 'pg', '~> 1.1'
+gem 'puma', '~> 5.0'
+gem 'sass-rails', '>= 6'
+gem 'bootsnap', '>= 1.4.4', require: false
+gem 'uglifier', '>= 1.3.0'
+gem 'autoprefixer-rails', '~> 10.0'
+gem 'dalli'
+gem 'rack-cors', require: 'rack/cors'
+gem 'sprockets', '~> 3.7.2'
+gem 'sucker_punch', '~> 2.0'
+
+group :development do
+  gem 'listen', '~> 3.3'
+  gem 'spring'
+  gem 'pry-rails'
+end
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,109 @@
+//////////////////////////////////////////////////////////////
+// App namespace
+//////////////////////////////////////////////////////////////
+
+window.App = window.App || {};
+
+App.pageLoad = [];
+App.pageResize = [];
+App.pageScroll = [];
+// App.pageThrottledScroll = [];
+// App.pageDebouncedResize = [];
+App.teardown = [];
+App.runFunctions = function(array) {
+  for (var i = array.length - 1; i >= 0; i--) {
+    array[i]();
+  }
+};
+App.isHomePage = function() {
+  return App.$body.hasClass('controller--home_pages');
+};
+
+//////////////////////////////////////////////////////////////
+// On page load
+//////////////////////////////////////////////////////////////
+
+$(function() {
+  App.scrollTop = $(window).scrollTop();
+
+  App.windowWidth  = $(window).width();
+  App.windowHeight = $(window).height();
+
+  App.$html = $('html');
+  App.$body = $('body');
+  App.$header = $('#header');
+
+  App.$html.removeClass('no-js');
+
+  App.runFunctions(App.pageLoad);
+  App.runFunctions(App.pageResize);
+  // App.runFunctions(App.pageDebouncedResize);
+  App.runFunctions(App.pageScroll);
+  // App.runFunctions(App.pageThrottledScroll);
+});
+
+//////////////////////////////////////////////////////////////
+// On scroll
+//////////////////////////////////////////////////////////////
+
+$(window).on('scroll', function() {
+  App.scrollTop = $(window).scrollTop();
+
+  App.runFunctions(App.pageScroll);
+});
+
+// $(window).on('scroll', $.throttle(200, function() {
+//   App.runFunctions(App.pageThrottledScroll);
+// }));
+
+//////////////////////////////////////////////////////////////
+// On resize
+//////////////////////////////////////////////////////////////
+
+$(window).on('resize', function() {
+  App.windowWidth  = $(window).width();
+  App.windowHeight = $(window).height();
+
+  App.runFunctions(App.pageResize);
+});
+
+// $(window).on('resize', $.debounce(500, function() {
+//   App.runFunctions(App.pageDebouncedResize);
+// }));
+
+App.breakpoint = function(checkIfSize) {
+  // Make sure these match the breakpoint variables set in variables.scss
+  var xs = 576;
+  var sm = 768;
+  var md = 992;
+  var lg = 1200;
+  var breakpoint;
+
+  if ( App.windowWidth < sm ) {
+    breakpoint = 'xs';
+  } else if ( App.windowWidth >= lg ) {
+    breakpoint = 'lg';
+  } else if ( App.windowWidth >= md ) {
+    breakpoint = 'md';
+  } else {
+    breakpoint = 'sm';
+  }
+
+  if ( checkIfSize !== undefined ) {
+    if ( checkIfSize == 'xs' ) {
+      return App.windowWidth < xs;
+    } else if ( checkIfSize == 'sm' ) {
+      return (App.windowWidth >= xs && App.windowWidth < sm);
+    } else if ( checkIfSize == 'md' ) {
+      return (App.windowWidth >= sm && App.windowWidth < md);
+    } else if ( checkIfSize == 'lg' ) {
+      return App.windowWidth >= md;
+    }
+  } else {
+    return breakpoint;
+  }
+};
+
+App.breakpoint.isMobile = function() {
+  return ( App.breakpoint('xs') || App.breakpoint('sm') );
+};
