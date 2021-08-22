55251e14fc800938c25dbd6d6d1977246be27359
dylanfisher <hi@dylanfisher.com>
2021-08-22 01:46:36 -0400

    Test out using custom cloudfront domain name

--- app/decorators/file_uploader_decorator.rb
+++ app/decorators/file_uploader_decorator.rb
@@ -5,13 +5,13 @@ FileUploader.class_eval do
 
   add_metadata do |io, derivative: nil, **|
     begin
-      return unless derivative.nil? && io.image? && (io.content_type == 'image/jpeg')
+      if derivative.nil? && io.image? && (io.content_type == 'image/jpeg')
+        exif_data = Shrine.with_file(io) do |file|
+          EXIFR::JPEG.new(file.path)
+        end
 
-      exif_data = Shrine.with_file(io) do |file|
-        EXIFR::JPEG.new(file.path)
+        exif_data.to_hash
       end
-
-      exif_data.to_hash
     rescue => e
       Rails.logger.error { e.message }
       Rails.logger.error { e.backtrace.first(10).join("\n") }
