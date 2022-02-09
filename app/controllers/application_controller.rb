0b59b4c5a41f9a02ee842fdc24b3430f40dce12a
dylanfisher <hi@dylanfisher.com>
2022-02-09 18:28:33 -0500

    Add http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :http_authenticate
+
+  private
+
+  def http_authenticate
+    unless Rails.env.development?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == 'memories' && password == 'memories'
+      end
+    end
+  end
 end
